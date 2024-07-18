2b0791d473683dddca8f14c1bffe36917c193d0e
Sergii Musiienko <vpnmus@ukr.net>
2024-07-18 23:04:02 +0300

    Setup Pillow, crispy, crispy_bootstrap4 and add relevant settings

--- m_taskmanager/settings.py
+++ m_taskmanager/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
