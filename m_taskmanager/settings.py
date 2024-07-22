160290d3cd2c72f53be9905d8caf2ca00d47300d
Sergii Musiienko <vpnmus@ukr.net>
2024-07-22 21:06:53 +0300

    added a feature to avoid the error

--- m_taskmanager/settings.py
+++ m_taskmanager/settings.py
@@ -90,7 +90,8 @@ DATABASES = {
 }
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
